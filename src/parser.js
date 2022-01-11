35a4ef63eec6aec04ae6214f0701ef7f5bbdc921
Nataly Lapina <lapinann08@gmail.com>
2022-01-11 15:47:13 +0300

    fix error of parse by parsererror

--- src/parser.js
+++ src/parser.js
@@ -1,30 +1,31 @@
 export default (xmlContent, url) => {
-  try {
-    const parser = new DOMParser();
-    const htmlContent = parser.parseFromString(xmlContent, 'application/xml');
-    const channel = htmlContent.querySelector('channel');
-    const feedTitle = channel.querySelector('title').textContent;
-    const feedDescription = channel.querySelector('description').textContent;
-    const feed = {
-      title: feedTitle,
-      description: feedDescription,
-      url,
-    };
-    const posts = Array.from(channel.querySelectorAll('item')).map((item) => {
-      const postTitle = item.querySelector('title').textContent;
-      const postDescription = item.querySelector('description').textContent;
-      const postLink = item.querySelector('link').textContent;
-      const post = {
-        title: postTitle,
-        description: postDescription,
-        link: postLink,
-      };
-      return post;
-    });
-    return { feed, posts };
-  } catch (err) {
-    const errorObject = new Error(err);
-    errorObject.isParsingError = true;
-    throw errorObject;
+  const parser = new DOMParser();
+  const htmlContent = parser.parseFromString(xmlContent, 'application/xml');
+  const parsererror = htmlContent.querySelector('parsererror');
+  if (parsererror) {
+    const errorMessage = parsererror.textContent;
+    const error = new Error(errorMessage);
+    error.isParsingError = true;
+    throw error;
   }
+  const channel = htmlContent.querySelector('channel');
+  const feedTitle = channel.querySelector('title').textContent;
+  const feedDescription = channel.querySelector('description').textContent;
+  const feed = {
+    title: feedTitle,
+    description: feedDescription,
+    url,
+  };
+  const posts = Array.from(channel.querySelectorAll('item')).map((item) => {
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    const postLink = item.querySelector('link').textContent;
+    const post = {
+      title: postTitle,
+      description: postDescription,
+      link: postLink,
+    };
+    return post;
+  });
+  return { feed, posts };
 };
